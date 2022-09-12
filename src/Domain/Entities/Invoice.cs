ddee92035ef3aea9e11fde2d3aa711f63bb343da
PyLukasz <as.lukasz@gmail.com>
2022-09-12 09:17:55 +0200

    Add properties to entity

--- src/Domain/Entities/Invoice.cs
+++ src/Domain/Entities/Invoice.cs
@@ -5,6 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanArchitecture.Domain.Entities;
-internal class Invoice
+public class Invoice
 {
+    public int Id { get; set; }
+    public string? NameOfCompanySeller { get; set; }
+    public string? NameOfCompanyBuyer { get; set; }
+    public string? NIP { get; set; }
+    public bool PaymentByBankTransfer { get; set; }
+    public string? InvoiceNumber { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime DateOfIssue { get; set; }
+    public DateTime SaleDate { get; set; }
+    public DateTime PaymentDeadline { get; set; }
 }
